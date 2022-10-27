33dfc56a4e55ee6ff899bd882448e1d348b848f3
Yaroslava Kozhemiako <nibiru.crs@gmail.com>
2022-10-27 12:34:43 +0300

    initial commit

---
+++ src/Lab6/Couchette.java
@@ -0,0 +1,11 @@
+package Lab6;
+
+public class Couchette extends RailroadCar {
+
+    public Couchette(int amountOfPassengers, int amountOfLuggage, int levelOfComfort) {
+        super(amountOfPassengers, amountOfLuggage, levelOfComfort);
+    }
+
+
+}
+
